627143534a074281db1c599ddb08cbe8ae094744
jorgearmas <jorgearmasc0496@gmail.com>
2025-03-13 09:49:01 -0600

    Form 3 handles table management

---
+++ SQL_Carpenter/Forms/Form3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_Carpenter.Forms
+{
+    public partial class Form3 : Form
+    {
+        public Form3(string db_name_from_form2)
+        {
+            InitializeComponent();
+            string db_name = db_name_from_form2;
+        }
+    }
+}
