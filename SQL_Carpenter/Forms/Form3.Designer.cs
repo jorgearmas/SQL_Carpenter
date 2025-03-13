647da8cbf500f19a32cb1a2b35523f19fcae40fc
jorgearmas <jorgearmasc0496@gmail.com>
2025-03-13 11:33:07 -0600

    txtbox, comboBox, button and panel added to handle table creation visual

--- SQL_Carpenter/Forms/Form3.Designer.cs
+++ SQL_Carpenter/Forms/Form3.Designer.cs
@@ -28,18 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            button1 = new Button();
+            panel1 = new Panel();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            comboBox1 = new ComboBox();
+            label4 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Underline, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Create table in DB:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(151, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(117, 46);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 49);
+            label3.Name = "label3";
+            label3.Size = new Size(88, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Table Name";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(117, 253);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Add Field";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(265, 163);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(491, 182);
+            panel1.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 163);
+            label5.Name = "label5";
+            label5.Size = new Size(85, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Field Name";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(117, 161);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "INT", "VARCHAR(50)", "DATE" });
+            comboBox1.Location = new Point(117, 204);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(125, 28);
+            comboBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 204);
+            label4.Name = "label4";
+            label4.Size = new Size(76, 20);
+            label4.TabIndex = 11;
+            label4.Text = "Field Type";
             // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1466, 395);
+            Controls.Add(label4);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Form3";
             Text = "Form3";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private Button button1;
+        private Panel panel1;
+        private Label label5;
+        private TextBox textBox3;
+        private ComboBox comboBox1;
+        private Label label4;
     }
 }
