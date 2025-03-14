34a2658e349dc6bb5e9229caf2c43eca1dd41d74
jorgearmas <jorgearmasc0496@gmail.com>
2025-03-14 10:54:46 -0600

    Added drop table method

--- SQL_Carpenter/Services/DDL/TableManagement.cs
+++ SQL_Carpenter/Services/DDL/TableManagement.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SQL_Carpenter.Data;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Newtonsoft.Json.Linq;
 
 namespace SQL_Carpenter.Services.DDL
 {
@@ -30,6 +31,38 @@ namespace SQL_Carpenter.Services.DDL
             int table_creation_status = command.ExecuteNonQuery();
             CloseConnection();
             return table_creation_status;
+        }
+
+        public List<JObject> GetAllTables(string db_name)
+        {
+            List<JObject> returnThese = new List<JObject>();
+            string query = $"USE {db_name}; SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'";
+            OpenConnection();
+            SqlCommand command = new SqlCommand(query, _connection);
+            using(SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    JObject newTableElement = new JObject();
+                    for(int i = 0; i < reader.FieldCount; i++)
+                    {
+                        newTableElement.Add(reader.GetName(i).ToString(), reader.GetValue(i).ToString());
+                    }
+                    returnThese.Add(newTableElement);
+                }
+            }
+            CloseConnection() ;
+            return returnThese;
+        }
+
+        public int DropTable(string db_name, string table_name)
+        {
+            string query = $"USE {db_name}; DROP TABLE {table_name};";
+            OpenConnection();
+            SqlCommand command = new SqlCommand (query, _connection);
+            int table_deletion_status = command.ExecuteNonQuery();
+            CloseConnection() ;
+            return table_deletion_status;
         }
     }
 }
