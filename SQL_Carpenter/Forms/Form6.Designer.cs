4f7561ac0f2b16f7a21b8e33172221d867bb341d
jorgearmas <jorgearmasc0496@gmail.com>
2025-03-21 16:00:51 -0600

    Form6 handles UPDATE operation

---
+++ SQL_Carpenter/Forms/Form6.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SQL_Carpenter.Forms
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            button1 = new Button();
+            panel2 = new Panel();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDark;
+            panel1.Location = new Point(12, 49);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(491, 299);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(294, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Select columns where data will be updated";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(409, 354);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Select";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ControlDark;
+            panel2.Location = new Point(524, 49);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(491, 299);
+            panel2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(921, 354);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 4;
+            button2.Text = "UPDATE";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form6
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1466, 395);
+            Controls.Add(button2);
+            Controls.Add(panel2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Name = "Form6";
+            Text = "Form6";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Button button1;
+        private Panel panel2;
+        private Button button2;
+    }
+}
---
+++ SQL_Carpenter/Forms/Form6.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SQL_Carpenter.Services.DML;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using CheckBox = System.Windows.Forms.CheckBox;
+
+namespace SQL_Carpenter.Forms
+{
+    public partial class Form6 : Form
+    {
+        private string _server_name;
+        private string _user_name;
+        private string _password;
+        private string _db_name;
+        private string _table_name;
+        string subQueryColumns = "";
+
+        public Dictionary<CheckBox, string> checkBoxes = new Dictionary<CheckBox, string>();
+        public Dictionary<CheckBox, string> checkedBoxes = new Dictionary<CheckBox, string>();
+        public Dictionary<TextBox, string> textBoxesWithValues = new Dictionary<TextBox, string>();
+        public List<Object> valuesToUpdate = new List<Object>();
+        public Form6(string server_name, string user_name, string password, string db_name, string table_name)
+        {
+            _server_name = server_name;
+            _user_name = user_name;
+            _password = password;
+            _db_name = db_name;
+            _table_name = table_name;
+
+            InitializeComponent();
+            TableDMLManagement tableDMLManagement = new TableDMLManagement(_server_name, _user_name, _password);
+            Dictionary<string, string> columnsToRender = tableDMLManagement.getColumns(_db_name, _table_name);
+            List<string> columnsDataType = new List<string>();
+
+            foreach (var item in columnsToRender)
+            {
+                System.Windows.Forms.CheckBox checkBox = new System.Windows.Forms.CheckBox();
+                checkBox.Name = $"{item.Key}";
+                checkBox.Text = $" Name: {item.Key} | DataType: {item.Value}";
+
+                panel1.Controls.Add(checkBox);
+                int offsetY = panel1.Controls.Count * 30;
+                checkBox.Location = new Point(10, offsetY);
+                checkBox.Width = 300;
+                panel1.Controls.Add(checkBox);
+
+                checkBoxes.Add(checkBox, item.Value);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var item in checkBoxes)
+            {
+                if (item.Key.Checked)
+                {
+                    checkedBoxes.Add(item.Key, item.Value);
+                }
+            }
+
+            foreach (var item in checkedBoxes)
+            {
+                System.Windows.Forms.TextBox textBox = new System.Windows.Forms.TextBox();
+                System.Windows.Forms.Label label = new System.Windows.Forms.Label();
+
+                label.Name = $"lbl_{item.Key.Name}";
+                label.Text = $"{item.Key.Name}";
+                textBox.Name = $"txt_{item.Key.Name}";
+
+                int offsetY = (panel2.Controls.Count / 2) * 40;
+                label.Location = new Point(10, offsetY);
+                label.Width = 100;
+                textBox.Location = new Point(label.Right + 10, offsetY);
+                textBox.Width = 200;
+                panel2.Controls.Add(label);
+                panel2.Controls.Add(textBox);
+
+                subQueryColumns += $"{item.Key.Name}, ";
+                textBoxesWithValues.Add(textBox, item.Value);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var item in textBoxesWithValues)
+            {
+                if (item.Value == "int")
+                {
+                    valuesToUpdate.Add(Convert.ToInt32(item.Key.Text));
+                }
+                else
+                {
+                    valuesToUpdate.Add(item.Key.Text);
+                }
+                //MessageBox.Show(item.Key.Text);
+            }
+            string modifiedSubQueryColumns = subQueryColumns.Substring(0, subQueryColumns.Length - 2);
+        }
+    }
+}
