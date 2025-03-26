f9895c804ced1899e1633adfe8fed952220efd8f
jorgearmas <jorgearmasc0496@gmail.com>
2025-03-26 11:11:48 -0600

    Button linked with delete service via new instance, parameters passed to new delete method

--- SQL_Carpenter/Forms/Form7.Designer.cs
+++ SQL_Carpenter/Forms/Form7.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -38,14 +39,25 @@
             dataGridView1.Location = new Point(12, 37);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(1178, 310);
+            dataGridView1.Size = new Size(1178, 283);
             dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1052, 335);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Delete Selected";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form7
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1466, 395);
+            Controls.Add(button1);
             Controls.Add(dataGridView1);
             Name = "Form7";
             Text = "Form7";
@@ -56,5 +68,6 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private Button button1;
     }
 }
--- SQL_Carpenter/Forms/Form7.cs
+++ SQL_Carpenter/Forms/Form7.cs
@@ -14,13 +14,33 @@ namespace SQL_Carpenter.Forms
     public partial class Form7 : Form
     {
         BindingSource getAllRecordsFromTableBindingSource = new BindingSource();
+        private string _server_name;
+        private string _user_name;
+        private string _password;
+        private string _db_name;
+        private string _table_name;
         public Form7(string server_name, string user_name, string password, string db_name, string table_name)
         {
+            _server_name = server_name;
+            _user_name = user_name;
+            _password = password;
+            _db_name = db_name;
+            _table_name = table_name;
             InitializeComponent();
-            TableDMLManagement tableDMLManagement = new TableDMLManagement(server_name, user_name, password);
+            TableDMLManagement tableDMLManagement = new TableDMLManagement(_server_name, _user_name, _password);
             getAllRecordsFromTableBindingSource.DataSource = tableDMLManagement.getAllRecords(db_name, table_name);
             dataGridView1.DataSource = getAllRecordsFromTableBindingSource;
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rowClicked = dataGridView1.CurrentRow.Index;
+            int ID_toDelete = Convert.ToInt32(dataGridView1.Rows[rowClicked].Cells[0].Value);
+            string IdColumnName = dataGridView1.Columns[0].Name;
+            
+            TableDMLManagement tableDMLManagement = new TableDMLManagement(_server_name, _user_name, _password);
+            int result = tableDMLManagement.deleteRow(_db_name, _table_name, IdColumnName, ID_toDelete);
+            MessageBox.Show(result.ToString());
+        }
     }
 }
