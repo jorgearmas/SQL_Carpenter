4f16f5eeff8d1c6f05e14c905b27e48d98f7e4d4
jorgearmas <jorgearmasc0496@gmail.com>
2025-04-11 17:40:08 -0600

    Created facade class to manage DDL services

---
+++ SQL_Carpenter/Managers/DatabaseManager.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using SQL_Carpenter.Services.DDL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Carpenter.Managers
+{
+    internal class DatabaseManager
+    {
+        public static List<JObject> GetAllDatabases()
+        {
+            var service = new GetAllDB();
+            return service.getAllDataBases();
+        }
+
+        public static int CreateDatabase(string databaseName)
+        {
+            var service = new CreateDB();
+            return service.CreateDatabase(databaseName);
+        }
+        public static int DeleteDatabase(string database_name)
+        {
+            var service = new DropDB();
+            return service.DropDatabase(database_name);
+        }
+    }
+}
